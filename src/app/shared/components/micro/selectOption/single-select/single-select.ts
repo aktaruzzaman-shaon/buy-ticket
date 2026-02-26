75adcc15f2055b909c6f73f9ea7f21dcff50b2f4
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2026-02-26 12:47:07 +0600

    added single-select component

---
+++ src/app/shared/components/micro/selectOption/single-select/single-select.ts
@@ -0,0 +1,43 @@
+import { Component, computed, input, output, signal } from '@angular/core';
+import { SelectOption } from './single-select.type';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OverlayModule } from '@angular/cdk/overlay';
+
+@Component({
+  selector: 'app-single-select',
+  imports: [CommonModule, FormsModule, OverlayModule],
+  templateUrl: './single-select.html',
+  styleUrl: './single-select.css',
+})
+export class SingleSelect {
+  options = input<SelectOption[]>([]);
+  placeholder = input<string>('Select...');
+  label = input<string>('From');
+
+  onSelection = output<SelectOption>();
+
+  searchTerm = signal('');
+  isOpen = signal(false);
+  selectedLabel = signal('');
+
+  filteredOptions = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+    const list = this.options();
+    if (!term) return list;
+    return list.filter((opt) => opt.label.toLowerCase().includes(term));
+  });
+
+  toggleDropdown() {
+    this.isOpen.update((v) => !v);
+    if (this.isOpen()) {
+      this.searchTerm.set('');
+    }
+  }
+
+  selectOption(option: SelectOption) {
+    this.selectedLabel.set(option.label);
+    this.isOpen.set(false);
+    this.onSelection.emit(option);
+  }
+}
